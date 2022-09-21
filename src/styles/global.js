d5cec19dba1c4eb6d218b9706a710f16876f325e
c.rafaf <c.rafaef@ternium.com.br>
2022-09-21 15:07:30 -0300

    modificacoes

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,13 +2,16 @@ import { createGlobalStyle } from "styled-components";
 
 const Global = createGlobalStyle`
 
-body {
+*{
     margin: 0;
     padding: 0;
-    font-family: 'Popins', Sans-serif;
-    background-color: red;
-    font-size:50px;
+}
+body {
     
+    font-family: 'Popins', Sans-serif;
+    background-color: black;
+    font-size:100px;
+    color: red
 }
 `;
 
