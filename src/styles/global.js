d896fec17b0f5f3f3138813b5d32a580d16f3b04
c.rafaf <c.rafaef@ternium.com.br>
2022-09-21 13:33:45 -0300

    arquivos-iniciais

---
+++ src/styles/global.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+
+body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Popins', Sans-serif;
+    background-color: red;
+    font-size:50px;
+    
+}
+`;
+
+
+
+
+
+export default Global;
