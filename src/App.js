c45e4d73b9ab926c6a9f08df05e377eff022dc7d
c.rafaf <c.rafaef@ternium.com.br>
2022-09-29 14:39:10 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -9,11 +9,17 @@ const App = () => {
   const [transactionsList, setTransactionsList] = useState(
     data ? JSON.parse(data) : []
   );
+  
   const [income, setIncome] = useState(0);
   const [expense, setExpense] = useState(0);
   const [total, setTotal] = useState(0);
+  const [date, setDate] = useState(0);
+ 
 
   useEffect(() => {
+
+    
+
     const amountExpense = transactionsList
       .filter((item) => item.expense)
       .map((transaction) => Number(transaction.amount));
@@ -24,12 +30,14 @@ const App = () => {
 
     const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
     const income = amountIncome.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+   
 
     const total = Math.abs(income - expense).toFixed(2);
 
     setIncome(`R$ ${income}`);
     setExpense(`R$ ${expense}`);
     setTotal(`${Number(income) < Number(expense) ? "-" : ""}R$ ${total}`);
+    
   }, [transactionsList]);
 
   const handleAdd = (transaction) => {
