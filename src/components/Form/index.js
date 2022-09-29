570702be4f33aa8d0067849adb7432b0050ec5a9
c.rafaf <c.rafaef@ternium.com.br>
2022-09-29 10:16:43 -0300

    criacao do campo data

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,72 +1,82 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
+import Grid from "../Grid";
 import * as C from "./styles";
 
-const Form = ({handleAdd, TransactionsList, setTransactionsList, }) => {
-    const [desc, setDesc] = useState("");
-    const [amount, setAmount]=useState("")  
-    const [isExpense, setExpense] = useState(false);
+const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
+  const [desc, setDesc] = useState("");
+  const [amount, setAmount] = useState("");
+  const [isExpense, setExpense] = useState(false);
 
-    const generateID = () => Math.round(Math.random() * 1000);
+  const generateID = () => Math.round(Math.random() * 1000);
 
-    const handleSave = ()=> {
-        if(!desc || !amount){
-            alert("Informe a descrição e o valor");
-            return;
-        }else if (amount < 1){
-            alert("o valor tem que ser positvo!")
-            return;
-        }
+  const handleSave = () => {
+    if (!desc || !amount) {
+      alert("Informe a descrição e o valor!");
+      return;
+    } else if (amount < 1) {
+      alert("O valor tem que ser positivo!");
+      return;
+    }
 
-        const transaction = {
-            id: generateID(),
-            desc:desc,
-            amount: amount,
-            expense: isExpense, 
-        };
-
-        handleAdd(transaction);
-
-        setDesc("");
-        setAmount("");
+    const transaction = {
+      id: generateID(),
+      desc: desc,
+      amount: amount,
+      expense: isExpense,
     };
 
-    return(
-        <>
-        <C.Container>
-            <C.InputContent>
-                <C.Label>Descrição</C.Label>
-                <C.Input value={desc} onChange={(e) => setDesc(e.target.value)}/>
-            </C.InputContent>
-            <C.InputContent>
-                <C.Label>Valor</C.Label>
-                <C.Input value={amount} type="number" onChange={(e) => setAmount(e.target.value)}
-                />
-            </C.InputContent>
-            <C.RadioGroup>
-                <C.Input 
-                type="radio" 
-                id= "rIncome" 
-                defaultChecked 
-                name="group1" 
-                onChange={() => setExpense(!isExpense)}
-                />
+    handleAdd(transaction);
 
-            <C.Label htmlFor="rIncome">Entrada</C.Label>
-                <C.Input 
-                type="radio" 
-                id= "rExpenses"                 
-                name="group1" 
-                onChange={() => setExpense(!isExpense)}
-                />
-            <C.Label htmlFor="rExpenses">Saída</C.Label>
+    setDesc("");
+    setAmount("");
+  };
 
-            </C.RadioGroup>
-            <C.Button onClick={handleSave}>Adicionar</C.Button>
-        
-        </C.Container>  
-        <Grid itens={transactionsList} setItens ={setTransactionsList}/> 
+  return (
+    <>
+      <C.Container>
+      <C.InputContent>
+          <C.Label>Data</C.Label>
+          <C.Input
+            value={amount}
+             type="date"
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </C.InputContent>
 
-        </>
-    );
+        <C.InputContent>
+          <C.Label>Descrição</C.Label>
+          <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} />
+        </C.InputContent>
+        <C.InputContent>
+          <C.Label>Valor</C.Label>
+          <C.Input
+            value={amount}
+            type="number"
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </C.InputContent>
+        <C.RadioGroup>
+          <C.Input
+            type="radio"
+            id="rIncome"
+            defaultChecked
+            name="group1"
+            onChange={() => setExpense(!isExpense)}
+          />
+          <C.Label htmlFor="rIncome">Entrada</C.Label>
+          <C.Input
+            type="radio"
+            id="rExpenses"
+            name="group1"
+            onChange={() => setExpense(!isExpense)}
+          />
+          <C.Label htmlFor="rExpenses">Saída</C.Label>
+        </C.RadioGroup>
+        <C.Button onClick={handleSave}>ADICIONAR</C.Button>
+      </C.Container>
+      <Grid itens={transactionsList} setItens={setTransactionsList} />
+    </>
+  );
 };
+
 export default Form;
--- src/components/GridItem/index.js
+++ src/components/GridItem/index.js
@@ -1,30 +1,28 @@
 import React from "react";
 import * as C from "./styles";
-import{
-    FaRegArrowCircleUp,
-    FaRegArrowCircleDown,
-    faTrash,
-    FaTrash,
-}from "react-icons/fa";
+import {
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaTrash,
+} from "react-icons/fa";
 
-const GridItem = ({item, onDelete}) => {
-    return(
-        <C.Tr>
-            <C.Td>{item.desc}</C.Td>
-            <C.Td>{item.amout}</C.Td>
-            <C.Td alignCenter>
-                {
-                    item.expense ? (
-                        <FaRegArrowCircleDown color="red"/>
-                        ) : (
-                        <FaRegArrowCircleUp color="green"/>
-                        )
-                }
-            </C.Td>
-            <C.Td alignCenter>
-                <FaTrash onClick={()=> onDelete(item.id)}/>
-            </C.Td>
-        </C.Tr>
-    );
+const GridItem = ({ item, onDelete }) => {
+  return (
+    <C.Tr>
+      <C.Td>{item.desc}</C.Td>
+      <C.Td>{item.amount}</C.Td>
+      <C.Td alignCenter>
+        {item.expense ? (
+          <FaRegArrowAltCircleDown color="red" />
+        ) : (
+          <FaRegArrowAltCircleUp color="green" />
+        )}
+      </C.Td>
+      <C.Td alignCenter>
+        <FaTrash onClick={() => onDelete(item.id)} />
+      </C.Td>
+    </C.Tr>
+  );
 };
+
 export default GridItem;
